fa8e71d0f0df8cefa61532daad1708aefae86f0b
Dean Wilson <dean.wilson@gmail.com>
2016-02-01 19:16:12 +0000

    Add the actual code
    spec files too

---
+++ lib/puppet-lint/plugins/world_writable_files.rb
@@ -0,0 +1,24 @@
+PuppetLint.new_check(:world_writable_files) do
+  def check
+    resource_indexes.each do |resource|
+      if resource[:type].value == 'file'
+        resource[:param_tokens].select { |param_token|
+          param_token.value == 'mode'
+        }.each do |param_token|
+
+          # get the file modes value
+          value_token = param_token.next_code_token.next_code_token
+
+          break if value_token.value =~ /\d+[^2367]$/
+
+          notify :warning, {
+            message: 'files should not be created with world writable permissions',
+            line:    value_token.line,
+            column:  value_token.column,
+            token:   value_token,
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/puppet-lint/plugins/puppet-lint-world_writable_files_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'world_writable_files' do
+  context 'file with a mode of 640' do
+    let(:code) do
+      <<-EOS
+        class locked_down_file {
+          file { '/tmp/locked_down':
+            ensure => 'file',
+            mode   => '0640',
+          }
+        }
+      EOS
+    end
+
+    it 'should not detect any problems' do
+      expect(problems).to have(0).problems
+    end
+  end
+
+  context 'file with a world writable octal mode of 666' do
+    let(:msg) { 'files should not be created with world writable permissions' }
+    let(:code) do
+      <<-EOS
+        class locked_down_file {
+          file { '/tmp/open_octal':
+            ensure => 'file',
+            mode   => '0666',
+          }
+        }
+      EOS
+    end
+
+    it 'should detect a problem' do
+      expect(problems).to have(1).problem
+    end
+
+    it 'should create a warning' do
+      expect(problems).to contain_warning(msg).on_line(4).in_column(23)
+    end
+  end
+end
