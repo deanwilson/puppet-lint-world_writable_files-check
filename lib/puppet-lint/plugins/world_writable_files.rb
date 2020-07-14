3e83e9149642bd90a00dd06e55ffc28df280661b
Dean Wilson <dean.wilson@gmail.com>
2020-07-14 08:18:48 +0100

    Satisfy rubocops checks

--- lib/puppet-lint/plugins/world_writable_files.rb
+++ lib/puppet-lint/plugins/world_writable_files.rb
@@ -1,25 +1,24 @@
 PuppetLint.new_check(:world_writable_files) do
   def check
     resource_indexes.each do |resource|
-      if resource[:type].value == 'file'
-        resource[:param_tokens].select { |param_token|
-          param_token.value == 'mode'
-        }.each do |param_token|
+      next unless resource[:type].value == 'file'
 
-          # get the file modes value
-          value_token = param_token.next_code_token.next_code_token
+      param_tokens = resource[:param_tokens].select { |pt| pt.value == 'mode' }
 
-          # we only work with octal for now - also stops file { mode => undef }
-          break if value_token.value !~ /^\d+$/
-          break if value_token.value =~ /\d+[^2367]$/
+      param_tokens.each do |param_token|
+        # get the file modes value
+        value_token = param_token.next_code_token.next_code_token
 
-          notify :warning, {
-            message: 'files should not be created with world writable permissions',
-            line:    value_token.line,
-            column:  value_token.column,
-            token:   value_token,
-          }
-        end
+        # we only work with octal for now - also stops file { mode => undef }
+        break if value_token.value !~ /^\d+$/
+        break if value_token.value =~ /\d+[^2367]$/
+
+        notify :warning, {
+          message: 'files should not be created with world writable permissions',
+          line:    value_token.line,
+          column:  value_token.column,
+          token:   value_token,
+        }
       end
     end
   end
